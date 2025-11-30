26cc48867938c83382fae7b563038dac701f8925
Sucipto <sucipto@pm.me>
2025-11-30 14:39:31 +0700

    bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Maui.MixProject do
   def project do
     [
       app: :maui,
-      version: "0.1.16",
+      version: "0.1.17",
       elixir: "~> 1.15",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
